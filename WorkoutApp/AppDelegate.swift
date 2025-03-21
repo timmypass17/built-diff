4759dab84bed98e0fc387932ee818369b57faf4b
timmypass17 <timmypass17@gmail.com>
2025-03-20 21:00:27 -0700

    added german localization

--- WorkoutApp/AppDelegate.swift
+++ WorkoutApp/AppDelegate.swift
@@ -19,7 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         WCSession.default.delegate = sessionDelegator
         WCSession.default.activate()
-        print(keyWords)
+        
+        printLocalizable()
         return true
     }
 
@@ -42,214 +43,94 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 // sucode swiftui exanded calendar
 
 var words = """
-    "-" : {
-
-    },
-    "%@" : {
-
-    },
-    "%@ %@" : {
-
-    },
-    "%lld %@" : {
-
-    },
-    "%lld Workouts" : {
-
-    },
-    "%lldx%d %@ at %@" : {
-
-    },
-    "+%@ %@" : {
-
-    },
-    "+0 %@" : {
-
-    },
-    "Accent Color" : {
-
-    },
-    "Add Exercise" : {
-
-    },
-    "Alphabetical (A-Z)" : {
-
-    },
-    "Appearance" : {
-
-    },
-    "Are you sure you want to delete \"%@\"" : {
-
-    },
-    "Automatic" : {
-
-    },
-    "Best: %@ %@" : {
-
-    },
-    "blue" : {
-
-    },
-    "brown" : {
-
-    },
-    "Bug Report" : {
-
-    },
-    "Cancel" : {
-
-    },
-    "Contact Us" : {
-
-    },
-    "Create Workout" : {
-
-    },
-    "Custom" : {
-
-    },
-    "cyan" : {
-
-    },
-    "Dark" : {
-
-    },
-    "Data Privacy" : {
-
-    },
-    "Date" : {
-
-    },
-    "Delete Template?" : {
-
-    },
-    "Delete Workout" : {
-
-    },
-    "Edit Workout" : {
-
-    },
-    "Exercises" : {
-
-    },
-    "General" : {
-
-    },
-    "green" : {
-
-    },
-    "Haptic Feedback" : {
-
-    },
-    "Help & Support" : {
-
-    },
-    "indigo" : {
-
-    },
-    "Latest: %@ %@" : {
-
-    },
-    "Light" : {
-
-    },
-    "Log" : {
-
-    },
-    "Metric (kg)" : {
-
-    },
-    "mint" : {
-
-    },
-    "No Email Account Found" : {
-
-    },
-    "OK" : {
-
-    },
-    "orange" : {
-
-    },
-    "pink" : {
-
-    },
-    "Position" : {
-
-    },
-    "Privacy" : {
-
-    },
-    "Privacy Policy" : {
-
-    },
-    "Progress" : {
-
-    },
-    "purple" : {
-
-    },
-    "Push Day" : {
-
-    },
-    "Recently Updated" : {
-
-    },
-    "red" : {
-
-    },
-    "Remove" : {
-
-    },
-    "Settings" : {
-
-    },
-    "Show Timer" : {
-
-    },
-    "Sort By" : {
-
-    },
-    "teal" : {
-
-    },
-    "Theme" : {
-
-    },
-    "There is no email account associated to this device. If you have any questions, please feel free to reach out to us at %@" : {
-
-    },
-    "Time" : {
-
-    },
-    "Title" : {
-
-    },
-    "Updated: %@" : {
-
-    },
-    "US/Imperial (lbs)" : {
-
-    },
-    "Weight" : {
-
-    },
-    "Weight Unit" : {
-
-    },
-    "Weight Units" : {
-
-    },
-    "white" : {
-
-    },
-    "Workout" : {
-
-    },
-    "yellow" : {
-
-    }
+-
+%@
+%@ %@
+%lld %@
+%lld Workouts
+%lldx%d %@ at %@
++%@ %@
++0 %@
+Accent Color
+Add Exercise
+Add Set
+Alphabetical (A-Z)
+Appearance
+Are you sure you want to delete %@
+Automatic
+Best: %@ %@
+blue
+brown
+Bug Report
+Cancel
+Contact Us
+Create Workout
+Custom
+cyan
+Dark
+Data Privacy
+Date
+Delete Log?
+Delete Template?
+Delete Workout
+Edit Workout
+Exercises
+Finish
+General
+green
+Haptic Feedback
+Help & Support
+indigo
+Latest: %@ %@
+Light
+Log
+Metric (kg)
+mint
+No Email Account Found
+OK
+orange
+pink
+Position
+PREVIOUS
+Privacy
+Privacy Policy
+Progress
+purple
+Push Day
+Recently Updated
+red
+Remove
+REPS
+Search Exercises
+SET
+Settings
+Show Timer
+Sort By
+teal
+Theme
+There is no email account associated to this device. If you have any questions, please feel free to reach out to us at %@
+Time
+Title
+Updated: %@
+US/Imperial (lbs)
+Weight
+Weight Unit
+Weight Units
+white
+Workout
+yellow
+Your workout data is locally stored on your device, ensuring complete privacy. No one else can access or view your data, guaranteeing the confidentiality of your personal fitness data.
 """
+
+func printLocalizable() {
+    if let url = Bundle.main.url(forResource: "Localizable", withExtension: "xcstrings"),
+        let stringsDict = NSDictionary(contentsOf: url) as? [String: Any] {
+        print(stringsDict)
+    } else {
+        print("fail")
+    }
+}
+
 
 var keyWords: String {
     words.replacingOccurrences(of: " : {", with: "")
@@ -259,4 +140,6 @@ var keyWords: String {
 }
 
 
-//Translate the following list of strings for an iOS application into {LANGUAGE}. Provide the result with the english word followed by a "-" and then the {LANGUAGE} equaivalent.
+
+//Translate the following list of strings for an iOS application into {LANGUAGE}. Provide the result with the english word followed by a "-" and then the {LANGUAGE} equivalent.
+
